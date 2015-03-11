b3d996c02a09dfca816327670a8f00a5e9fd7b49
zesix <yang.pulse@impulselimited.com>
2015-03-11 13:42:40 -0500

    Added Music Volume Slider script.

---
+++ Assets/Scripts/MusicVolumeSlider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MusicVolumeSlider : MonoBehaviour {
+	// Set slider value equal to music manager value.
+	void Awake () {
+		GetComponent<Slider>().value = MusicManager.Instance.Volume;
+	}
+
+	// method to update things on the scene and store the new value
+	public void ChangeValue (float value) {
+		MusicManager.Instance.Volume = value;
+	}
+
+}
