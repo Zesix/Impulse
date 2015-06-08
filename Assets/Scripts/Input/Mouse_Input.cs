b1f8b0e4ed561cb1c01adf1160c124e78565674c
zesix <yang.pulse@impulselimited.com>
2015-06-08 15:09:31 -0500

    Updated Mouse_Input

--- Assets/Scripts/Input/Mouse_Input.cs
+++ Assets/Scripts/Input/Mouse_Input.cs
@@ -20,51 +20,26 @@ using System.Collections;
 
 public class Mouse_Input : BaseInputController
 {
-	private Vector2 prevMousePos;
-	private Vector2 mouseDelta;
-	private float speedX = 0.05f;
-	private float speedY = 0.1f;
-	
-	public void Start ()
-	{
-		prevMousePos = Input.mousePosition;
-	}
-	
-	public override void CheckInput ()
-	{
-		// get input data from vertical and horizontal axis and store them internally in vert and horz so we don't
-		// have to access them every time we need to relay input data out
-		
-		// calculate a percentage amount to use per pixel
-		float scalerX = 100f / Screen.width;
-		float scalerY = 100f / Screen.height;
-		
-		// calculate and use deltas
-		float mouseDeltaY = Input.mousePosition.y - prevMousePos.y;
-		float mouseDeltaX = Input.mousePosition.x - prevMousePos.x;
-		
-		// scale based on screen size
-		vert += (mouseDeltaY * speedY) * scalerY;
-		horz += (mouseDeltaX * speedX) * scalerX;
-		
-		// store this mouse position for the next time we're here
-		prevMousePos = Input.mousePosition;
-		
-		// set up some boolean values for up, down, left and right
-		Up = (vert > 0);
-		Down = (vert < 0);
-		Left = (horz < 0);
-		Right = (horz > 0);    
-		
-		// REMEMBER: To make this work, you need to change some input settings. In Input settings, remove mouse 0 from Fire1 and change mouse 1 in Fire2 to mouse 0
-		
-		// get fire / action buttons
-		Fire1 = Input.GetButton ("Fire2");
-	}
-	
-	public void LateUpdate ()
-	{
-		// check inputs each LateUpdate() ready for the next tick
-		CheckInput ();
-	}
+    // Player input
+    [SerializeField]
+    Vector2 mouseInput;
+    [SerializeField]
+    float CameraDistance = 10.0f;
+
+    public override void CheckInput()
+    {
+        // Get mouse position
+        Vector3 mouse3DInput = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+                                                                        CameraDistance));
+        mouseInput = new Vector2(mouse3DInput.x, mouse3DInput.y);
+
+        // get fire / action buttons
+        Fire1 = Input.GetButton("Fire1");
+    }
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
 }
