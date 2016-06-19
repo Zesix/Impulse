34e39a7d9bb8220b73fff47de9e0bc944eac0af1
Zesix <yang.pulse@impulselimited.com>
2016-06-19 17:02:46 -0500

    Updated Notification system to be more performant in edge-cases.

--- Assets/Scripts/Utility/Notifications/NotificationCenter.cs
+++ Assets/Scripts/Utility/Notifications/NotificationCenter.cs
@@ -1,41 +1,44 @@
 ﻿using UnityEngine;
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using SenderTable = System.Collections.Generic.Dictionary<System.Object, System.Collections.Generic.List<System.EventHandler>>;
 
 /// <summary>
-/// Enables objects to subscribe to and post events.
+/// This delegate is similar to an EventHandler:
+///     The first parameter is the sender, 
+///     The second parameter is the arguments / info to pass
 /// </summary>
+using Handler = System.Action<System.Object, System.Object>;
+
+/// <summary>
+/// The SenderTable maps from an object (sender of a notification), 
+/// to a List of Handler methods
+///     * Note - When no sender is specified for the SenderTable, 
+///         the NotificationCenter itself is used as the sender key
+/// </summary>
+using SenderTable = System.Collections.Generic.Dictionary<System.Object, System.Collections.Generic.List<System.Action<System.Object, System.Object>>>;
+
 public class NotificationCenter
 {
+    #region Properties
+    /// <summary>
+    /// The dictionary "key" (string) represents a notificationName property to be observed
+    /// The dictionary "value" (SenderTable) maps between sender and observer sub tables
+    /// </summary>
     private Dictionary<string, SenderTable> _table = new Dictionary<string, SenderTable>();
+    private HashSet<List<Handler>> _invoking = new HashSet<List<Handler>>();
+    #endregion
 
     #region Singleton Pattern
-    public readonly static NotificationCenter instance = new NotificationCenter();
+    public static readonly NotificationCenter Instance = new NotificationCenter();
     private NotificationCenter() { }
     #endregion
 
-    private SenderTable GetSenderTable(string notificationName)
-    {
-        if (!_table.ContainsKey(notificationName))
-            _table.Add(notificationName, new SenderTable());
-        return _table[notificationName];
-    }
-
-    private List<EventHandler> GetObservers(SenderTable subTable, System.Object sender)
-    {
-        if (!subTable.ContainsKey(sender))
-            subTable.Add(sender, new List<EventHandler>());
-        return subTable[sender];
-    }
-
-    public void AddObserver(EventHandler handler, string notificationName)
+    #region Public
+    public void AddObserver(Handler handler, string notificationName)
     {
         AddObserver(handler, notificationName, null);
     }
 
-    public void AddObserver(EventHandler handler, string notificationName, System.Object sender)
+    public void AddObserver(Handler handler, string notificationName, System.Object sender)
     {
         if (handler == null)
         {
@@ -49,26 +52,36 @@ public class NotificationCenter
             return;
         }
 
-        SenderTable subTable = GetSenderTable(notificationName);
+        if (!_table.ContainsKey(notificationName))
+            _table.Add(notificationName, new SenderTable());
+
+        SenderTable subTable = _table[notificationName];
+
         System.Object key = (sender != null) ? sender : this;
-        List<EventHandler> list = GetObservers(subTable, key);
+
+        if (!subTable.ContainsKey(key))
+            subTable.Add(key, new List<Handler>());
+
+        List<Handler> list = subTable[key];
         if (!list.Contains(handler))
+        {
+            if (_invoking.Contains(list))
+                subTable[key] = list = new List<Handler>(list);
+
             list.Add(handler);
+        }
     }
 
-    public void RemoveObserver(EventHandler handler)
+    public void RemoveObserver(Handler handler, string notificationName)
     {
-        string[] keys = new string[_table.Keys.Count];
-        _table.Keys.CopyTo(keys, 0);
-        for (int i = keys.Length - 1; i >= 0; --i)
-            RemoveObserver(handler, keys[i]);
+        RemoveObserver(handler, notificationName, null);
     }
 
-    public void RemoveObserver(EventHandler handler, string notificationName)
+    public void RemoveObserver(Handler handler, string notificationName, System.Object sender)
     {
         if (handler == null)
         {
-            Debug.LogError("Can't remove a null event handler from notification");
+            Debug.LogError("Can't remove a null event handler for notification, " + notificationName);
             return;
         }
 
@@ -82,41 +95,44 @@ public class NotificationCenter
         if (!_table.ContainsKey(notificationName))
             return;
 
-        System.Object[] keys = new object[_table[notificationName].Keys.Count];
-        _table[notificationName].Keys.CopyTo(keys, 0);
-        for (int i = keys.Length - 1; i >= 0; --i)
-            RemoveObserver(handler, notificationName, keys[i]);
-    }
-
-    public void RemoveObserver(EventHandler handler, string notificationName, System.Object sender)
-    {
-        if (string.IsNullOrEmpty(notificationName))
-        {
-            Debug.LogError("A notification name is required to stop observation");
-            return;
-        }
-
-        // No need to take action if we dont monitor this notification
-        if (!_table.ContainsKey(notificationName))
-            return;
-
-        SenderTable subTable = GetSenderTable(notificationName);
+        SenderTable subTable = _table[notificationName];
         System.Object key = (sender != null) ? sender : this;
+
         if (!subTable.ContainsKey(key))
             return;
-        List<EventHandler> list = GetObservers(subTable, key);
-        for (int i = list.Count - 1; i >= 0; --i)
+
+        List<Handler> list = subTable[key];
+        int index = list.IndexOf(handler);
+        if (index != -1)
         {
-            if (list[i] == handler)
-            {
-                list.RemoveAt(i);
-                break;
-            }
+            if (_invoking.Contains(list))
+                subTable[key] = list = new List<Handler>(list);
+            list.RemoveAt(index);
         }
-        if (list.Count == 0)
+    }
+
+    public void Clean()
+    {
+        string[] notKeys = new string[_table.Keys.Count];
+        _table.Keys.CopyTo(notKeys, 0);
+
+        for (int i = notKeys.Length - 1; i >= 0; --i)
         {
-            subTable.Remove(key);
-            if (subTable.Count == 0)
+            string notificationName = notKeys[i];
+            SenderTable senderTable = _table[notificationName];
+
+            object[] senKeys = new object[senderTable.Keys.Count];
+            senderTable.Keys.CopyTo(senKeys, 0);
+
+            for (int j = senKeys.Length - 1; j >= 0; --j)
+            {
+                object sender = senKeys[j];
+                List<Handler> handlers = senderTable[sender];
+                if (handlers.Count == 0)
+                    senderTable.Remove(sender);
+            }
+
+            if (senderTable.Count == 0)
                 _table.Remove(notificationName);
         }
     }
@@ -128,14 +144,14 @@ public class NotificationCenter
 
     public void PostNotification(string notificationName, System.Object sender)
     {
-        PostNotification(notificationName, sender, EventArgs.Empty);
+        PostNotification(notificationName, sender, null);
     }
 
-    public void PostNotification(string notificationName, System.Object sender, EventArgs e)
+    public void PostNotification(string notificationName, System.Object sender, System.Object e)
     {
         if (string.IsNullOrEmpty(notificationName))
         {
-            Debug.LogError("A notification name is required to stop observation");
+            Debug.LogError("A notification name is required");
             return;
         }
 
@@ -144,20 +160,25 @@ public class NotificationCenter
             return;
 
         // Post to subscribers who specified a sender to observe
-        SenderTable subTable = GetSenderTable(notificationName);
+        SenderTable subTable = _table[notificationName];
         if (sender != null && subTable.ContainsKey(sender))
         {
-            List<EventHandler> handlers = GetObservers(subTable, sender);
-            for (int i = handlers.Count - 1; i >= 0; --i)
+            List<Handler> handlers = subTable[sender];
+            _invoking.Add(handlers);
+            for (int i = 0; i < handlers.Count; ++i)
                 handlers[i](sender, e);
+            _invoking.Remove(handlers);
         }
 
         // Post to subscribers who did not specify a sender to observe
         if (subTable.ContainsKey(this))
         {
-            List<EventHandler> handlers = GetObservers(subTable, this);
-            for (int i = handlers.Count - 1; i >= 0; --i)
+            List<Handler> handlers = subTable[this];
+            _invoking.Add(handlers);
+            for (int i = 0; i < handlers.Count; ++i)
                 handlers[i](sender, e);
+            _invoking.Remove(handlers);
         }
     }
+    #endregion
 }
--- Assets/Scripts/Utility/Notifications/ObjectExtensions.cs
+++ Assets/Scripts/Utility/Notifications/ObjectExtensions.cs
@@ -1,40 +1,34 @@
-﻿using System;
-using System.Collections;
+﻿using Handler = System.Action<System.Object, System.Object>;
 
-public static class ObjectExtensions
+public static class NotificationExtensions
 {
     public static void PostNotification(this object obj, string notificationName)
     {
-        NotificationCenter.instance.PostNotification(notificationName, obj);
+        NotificationCenter.Instance.PostNotification(notificationName, obj);
     }
 
-    public static void PostNotification(this object obj, string notificationName, EventArgs e)
+    public static void PostNotification(this object obj, string notificationName, object e)
     {
-        NotificationCenter.instance.PostNotification(notificationName, obj, e);
+        NotificationCenter.Instance.PostNotification(notificationName, obj, e);
     }
 
-    public static void AddObserver(this object obj, EventHandler handler, string notificationName)
+    public static void AddObserver(this object obj, Handler handler, string notificationName)
     {
-        NotificationCenter.instance.AddObserver(handler, notificationName);
+        NotificationCenter.Instance.AddObserver(handler, notificationName);
     }
 
-    public static void AddObserver(this object obj, EventHandler handler, string notificationName, object sender)
+    public static void AddObserver(this object obj, Handler handler, string notificationName, object sender)
     {
-        NotificationCenter.instance.AddObserver(handler, notificationName, sender);
+        NotificationCenter.Instance.AddObserver(handler, notificationName, sender);
     }
 
-    public static void RemoveObserver(this object obj, EventHandler handler)
+    public static void RemoveObserver(this object obj, Handler handler, string notificationName)
     {
-        NotificationCenter.instance.RemoveObserver(handler);
+        NotificationCenter.Instance.RemoveObserver(handler, notificationName);
     }
 
-    public static void RemoveObserver(this object obj, EventHandler handler, string notificationName)
+    public static void RemoveObserver(this object obj, Handler handler, string notificationName, System.Object sender)
     {
-        NotificationCenter.instance.RemoveObserver(handler, notificationName);
-    }
-
-    public static void RemoveObserver(this object obj, EventHandler handler, string notificationName, System.Object sender)
-    {
-        NotificationCenter.instance.RemoveObserver(handler, notificationName, sender);
+        NotificationCenter.Instance.RemoveObserver(handler, notificationName, sender);
     }
 }
