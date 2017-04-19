2449a0119a2790bab53b3900cae72d9691662e19
nanoziros <nero.lucho92@gmail.com>
2017-04-19 15:56:54 -0400

    Newtonsoft json parser integration

--- Assets/Scripts/Inventory/JSONReader.cs
+++ Assets/Scripts/Inventory/JSONReader.cs
@@ -1,30 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using SimpleJSON;
+using Newtonsoft.Json;
 using UnityEngine;
 
 public class JSONReader : MonoBehaviour
 {
-
     public static List<Dictionary<string, object>> Read(string file)
     {
         var list = new List<Dictionary<string, object>>();
         TextAsset data = Resources.Load(file) as TextAsset;
 
         if (data == null) return null;
-        JSONNode parsedJason = SimpleJSON.JSON.Parse(data.text);
-        foreach (JSONNode element in parsedJason.Children)
+        ItemData[] parsedJason = JsonConvert.DeserializeObject<ItemData[]>(data.text);
+        foreach (ItemData info in parsedJason)
         {
             Dictionary<string, object> entry = new Dictionary<string, object>();
-            entry.Add("NAME",(string)element["NAME"]);
-            entry.Add("RANGE", (int)element["RANGE"]);
-            entry.Add("ATTACK", (int)element["ATTACK"]);
-            entry.Add("DEFENSE", (int)element["DEFENSE"]);
-            entry.Add("DURABILITY", (int)element["DURABILITY"]);
-            entry.Add("COST", (int)element["COST"]);
+            entry.Add("NAME", (string)info.Name);
+            entry.Add("RANGE", (int)info.Range);
+            entry.Add("ATTACK", (int)info.Attack);
+            entry.Add("DEFENSE", (int)info.Defense);
+            entry.Add("DURABILITY", (int)info.Durability);
+            entry.Add("COST", (int)info.Cost);
             list.Add(entry);
         }
-
         return list;
     }
 }
