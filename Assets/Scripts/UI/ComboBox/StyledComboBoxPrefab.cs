cd0ab9ae714cefa9ef5285188698c80e79dcfd4d
Zesix <yang.pulse@impulselimited.com>
2018-02-11 18:16:04 -0600

    Remove ComboBox scripts since UGUI provides a Dropdown component. Modify default text in Menu sample scene to OpenSansBold.

--- Assets/Scripts/UI/ComboBox/StyledComboBoxPrefab.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class StyledComboBoxPrefab : MonoBehaviour 
-{
-	public RectTransform    MenuItem;	// where to put the menu button
-	public RectTransform 	ItemRoot;	// where to put the items list
-	public CanvasGroup 		ItemPanel;	// panel to toggle alpha on for open/close
-}
