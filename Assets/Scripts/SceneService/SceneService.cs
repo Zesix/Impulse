6725f4d43314b2c0e05f8c7d5135e28d43a10651
nanoziros <nero.lucho92@gmail.com>
2018-12-09 13:40:00 -0300

    Refactor SceneUnitlity static class

--- Assets/Scripts/SceneService/SceneService.cs
+++ Assets/Scripts/SceneService/SceneService.cs
@@ -256,33 +256,4 @@ namespace Impulse
 
         #endregion
     }
-
-    public static class SceneUtilityEx
-    {
-        public static string GetNextSceneName()
-        {
-            var nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
-
-            if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
-            {
-                return GetSceneNameByBuildIndex(nextSceneIndex);
-            }
-
-            return string.Empty;
-        }
-
-        private static string GetSceneNameByBuildIndex(int buildIndex)
-        {
-            return GetSceneNameFromScenePath(SceneUtility.GetScenePathByBuildIndex(buildIndex));
-        }
-
-        private static string GetSceneNameFromScenePath(string scenePath)
-        {
-            // Unity's asset paths always use '/' as a path separator
-            var sceneNameStart = scenePath.LastIndexOf("/", StringComparison.Ordinal) + 1;
-            var sceneNameEnd = scenePath.LastIndexOf(".", StringComparison.Ordinal);
-            var sceneNameLength = sceneNameEnd - sceneNameStart;
-            return scenePath.Substring(sceneNameStart, sceneNameLength);
-        }
-    }
 }
